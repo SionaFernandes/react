46e44bdf358b0c8872c4565e7b283d6e1ea68959
Siona Fernandes <sionac.fernandes@gmail.com>
2020-02-28 22:08:42 +0530

    states

--- src/App.js
+++ src/App.js
@@ -3,13 +3,50 @@ import "./App.css";
 import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "123", age: 26 },
+      { name: "456", age: 12 },
+      { name: "789", age: 25 }
+    ]
+  };
+
+  switchNameHandler = () => {
+    // console.log("clicked");
+    //not supported : this.state.persons[0].name = "112";
+    this.setState({
+      persons: [
+        { name: "123333", age: 26 },
+        { name: "456555", age: 12 },
+        { name: "789999", age: 25 }
+      ]
+    });
+  };
   render() {
     return (
       <div className="App">
         <h1>React app</h1>
         <p>Testing React</p>
+        <button onClick={this.switchNameHandler}>Switch Button</button>
+
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
+
+        {/* hard-coded */}
         <Person name="Abc" age="20" />
-        <Person name="Pqr" age="26" />
+        <Person name="Pqr" age="26">
+          Hobbies:Coding
+        </Person>
         <Person name="Xyz" age="30" />
       </div>
     );
